75535a36bd5dafa43bb049c6c42015b21e357c6e
Sancarn <sancarn@hotmail.co.uk>
2018-06-22 00:54:59 +0100

    Created - Variant Spec. Not yet fully implemented.

--- src/libs/generic/Variant/Variant.cr
+++ src/libs/generic/Variant/Variant.cr
@@ -1,3 +1,33 @@
+# Variant Spec:
+# An attempt at a fully dynamic type
+# VariantNAC is the Dynamic Type alone, Variant however
+# will also try to auto-cast itself to a compatible value
+#
+# # Always prioritise casting of variant
+# Variant.new("1") + 1 ==> 2
+# 1 + Variant.new("1") ==> 2
+# "1" + Variant.new(1) ==> "11"
+# Variant.new(1) + "1" ==> "11"
+#
+# # As above when Variant meets VariantNAC:
+# Variant.new("1") + VariantNAC.new(1) ==> 2
+# VariantNAC.new(1) + Variant.new("1") ==> 2
+# VariantNAC.new("1") + Variant.new(1) ==> "11"
+# Variant.new(1) + VariantNAC.new("1") ==> "11"
+#
+# # Wherever a Variant meets another Variant,
+# always prioritise the first value:
+# Variant.new(1) + Variant.new("1") ==> Variant.new(2)
+# Variant.new("1") + Variant.new(1) ==> Variant.new("11")
+#
+# # Allow possibility to extend to other non-standard classes
+#
+# # Possible a further VariantPreserve class should be created to store all possible values. E.G.:
+# (VariantP.new(1) + VariantP.new("1")).to_i ==> 2
+# (VariantP.new(1) + VariantP.new("1")).to_s ==> "11"
+# (VariantP.new(1) + "1").to_i ==> 2
+# (VariantP.new(1) + "1").to_s ==> "11"
+
 macro variant_wrap_math(t)
   struct Number
     def +(val : Variant({{t}}))
@@ -33,8 +63,26 @@ variant_wrap_math Float64
 variant_wrap_math Number
 
 class Variant(T)
-  @@auto_cast = false
+  def initialize(var : T)
+    @var = var
+  end
+  def to_s(io : IO)
+    io << @var.to_s
+  end
+  def +(val)
+    var = @var
+    if var.is_a? String
+      casted_arg = val.to_s
+      return var + casted_arg
+    else
+      casted_arg = typeof(var).new(val)
+      return var + casted_arg
+    end
+  end
+  forward_missing_to @var
+end
 
+class VariantNAC(T)
   getter thisType
   def initialize(var : T)
     @var = var
@@ -44,18 +92,5 @@ class Variant(T)
     io << @var.to_s
   end
 
-  #Autocasting
-  def self.auto_cast
-    @@auto_cast
-  end
-  def self.auto_cast=(val)
-    @@auto_cast=val
-  end
-  def self.auto_cast(&block)
-    current = @@auto_cast
-    @@auto_cast = true
-      block.call
-    @@auto_cast = current
-  end
   forward_missing_to @var
 end
